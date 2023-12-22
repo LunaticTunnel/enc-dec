5712c324291bbd277d329d56a5c1787549548a02
LunaticTunnel <153975030+LunaticTunnel@users.noreply.github.com>
2023-12-22 23:48:30 +0700

    Create obs.sh

---
+++ obs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+echo -e "\033[92;1m________________________________\033[0m"
+echo -e "\033[1;96m         LT PROJECT \033[0m"
+echo -e "\033[92;1m________________________________\033[0m"
+echo "*
+echo -e "\033[93[1] menu enc  dec \033[0m"
+echo -e ""
+read -p "pilih >>   " vc
+case $vc in
+1) clear ; wget -q  https://raw.githubusercontent.com/anubhavanonymous/Bash_Frustator/main/frustator.py && chmod +x frustator.py
+python2 frustator.py ;;
+*) exit 
+;;
+esac
